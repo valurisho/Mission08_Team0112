b9287594353fbd98f20bb97166154f2ae132b707
valurisho <153689011+valurisho@users.noreply.github.com>
2025-02-26 10:23:58 -0700

    Just add controller for Quadrant

--- Mission08_Team0112/Controllers/HomeController.cs
+++ Mission08_Team0112/Controllers/HomeController.cs
@@ -6,26 +6,13 @@ namespace Mission08_Team0112.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
 
     public IActionResult Index()
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    public IActionResult Quadrant()
     {
         return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
